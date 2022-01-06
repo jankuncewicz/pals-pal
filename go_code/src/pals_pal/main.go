04e44e43a6382d581e202c1c2e517577352b3bbf
Jan Kuncewicz <kuncewiczjan@gmail.com>
2022-01-06 11:48:56 +0100

    cylinder calculation almost ready to show

--- go_code/src/pals_pal/main.go
+++ go_code/src/pals_pal/main.go
@@ -4,11 +4,30 @@ import (
 	"fmt"
 	"pals_pal/bessel"
 	"pals_pal/ete"
+	"strconv"
+	"syscall/js"
 )
 
+func getFloat(val string, doc js.Value) float64 {
+	ans, _ := strconv.ParseFloat(doc.Call("getElementById", val).Get("value").String(), 64)
+	return ans
+}
+
+func getInt(val string, doc js.Value) int {
+	ans, _ := strconv.Atoi(doc.Call("getElementById", val).Get("value").String())
+	return ans
+}
+
 func main() {
-	n := 50
-	zeros := bessel.Zeros(n, n)
-	ints := bessel.IntegralList(n, n, zeros)
-	fmt.Println(ete.ApproxR(21.1, n, n, 0.193, 300, zeros, ints))
+	document := js.Global().Get("document")
+	tau := getFloat("tau", document)
+	T := getFloat("temp", document)
+	delta := getFloat("delta", document)
+	n := getInt("n", document)
+	m := getInt("m", document)
+	zeros := bessel.Zeros(n, m)
+	ints := bessel.IntegralList(n, m, zeros)
+	ans := ete.ApproxR(tau, n, m, delta, T, zeros, ints)
+	fmt.Println(ans)
+	document.Call("getElementById", "ans").Set("value", ans)
 }
